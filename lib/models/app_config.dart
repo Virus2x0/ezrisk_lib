d29de105f52af8242dcf680ed2a5f4916214e230
pathan-faizan <88704534+pathan-faizan@users.noreply.github.com>
2022-07-14 18:04:32 +0530

    Update app_config.dart

--- lib/models/app_config.dart
+++ lib/models/app_config.dart
@@ -1,3 +1,33 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 class JsonServer {
-  static String url = "http://127.0.0.1:3000/";
+   String countryId;
+  
+   JsonServer({required this.countryId});
+   String mainurl = "https://ezapi.jemistry.live/";
+  
+
+  countryData() async {
+  
+
+    var url = Uri.parse( mainurl + "country/" + countryId );
+    
+    var response = await http.get(url);
+    
+    var JsonDecode = jsonDecode(response.body);
+    
+    return JsonDecode;
+    
+}
+
+   infoData() async {
+      var url = Uri.parse(mainurl + "country/documents/" + countryId );
+    var response = await http.get(url);
+    var JsonDecode = jsonDecode(response.body);
+    return JsonDecode;
+
+}
+
 }
