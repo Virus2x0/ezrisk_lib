5c00b9b7c7fdb7e2b664af5b4f4d68fa4d526598
pathan-faizan <88704534+pathan-faizan@users.noreply.github.com>
2022-07-14 18:02:05 +0530

    Update detailFiles.dart

--- lib/pages/detailFiles.dart
+++ lib/pages/detailFiles.dart
@@ -1,3 +1,4 @@
+ import 'package:ezrisk/models/app_config.dart';
 import 'package:ezrisk/pages/pdf_page.dart';
 import 'package:ezrisk/pages/widget/page_title.dart';
 
@@ -9,28 +10,33 @@ import 'faq_page.dart';
 
 class DetailsFile extends StatefulWidget {
   // final userSec;
-  final DummyTitle;
-  List laws = ["Law 1", "Law 2", "Law 3", "Law 4"];
+  final sec_id;
 
-  DetailsFile({Key? key, required this.DummyTitle}) : super(key: key);
+  
+  List laws = ["not having data", "not having data", "not having data", "not having data"];
+
+  DetailsFile({Key? key, required this.sec_id}) : super(key: key);
 
   @override
   State<DetailsFile> createState() => _DetailsFileState();
 }
 
 class _DetailsFileState extends State<DetailsFile> {
-  // late List _users = [];
+   var type ;
+  late List _users = [];
 
   void loadData() async {
     // String sec_id = widget.userSec;
     // String country = "country/";
-
-    // var url = Uri.parse(JsonServer.url + country + sec_id);
-    // var response = await http.get(url);
-    // var JsonDecode = jsonDecode(response.body);
-    // print(url);
-    // _users = JsonDecode;
-
+    String sec_id = widget.sec_id ;
+    var data = JsonServer(countryId: sec_id);
+   var typeData = await data.infoData() ;
+   
+   
+   type = typeData['type'];
+    
+   _users = typeData['results']   ;
+   
     setState(() {});
   }
 
@@ -68,8 +74,8 @@ class _DetailsFileState extends State<DetailsFile> {
           padding: EdgeInsets.all(12),
           child: Column(
             children: [
-              DTitle(title: "${widget.DummyTitle}"),
-              //Image.network(widget.country.c_icon),
+              DTitle(title: "${type!=null ? type : "loading...."}"),
+              // Image.network(widget.country.c_icon),
               DetailsList()
             ],
           ),
@@ -80,11 +86,13 @@ class _DetailsFileState extends State<DetailsFile> {
     return Expanded(
       child: SingleChildScrollView(
         child: ListView.builder(
-          // itemCount: _users.length,
-          itemCount: widget.laws.length,
+          itemCount: _users.length,
+          // itemCount: widget.laws.length,
           physics: NeverScrollableScrollPhysics(),
           shrinkWrap: true,
           itemBuilder: (context, index) {
+            String PDFtitle = _users[index]['name'];
+            String PDFlink = _users[index]['link'] ;
             return Card(
                 elevation: 8,
                 shadowColor: Colors.blueAccent,
@@ -93,12 +101,16 @@ class _DetailsFileState extends State<DetailsFile> {
                   //!! yahha likhnaa
                   // title: "${_users[index]['rules']}".text.bold.xl2.make(),
                   // subtitle: "${_users[index]['regulation']}".text.make(),
-                  title: "${widget.laws[index]}".text.bold.xl2.make(),
+                  title: "${PDFtitle}".text.bold.xl2.make(),
 
                   trailing: ElevatedButton(
                     child: "  Open PDF ".text.make(),
                     onPressed: () {
-                      Get.to(() => PDFPage());
+                       Navigator.push(
+                       context,
+                       MaterialPageRoute(builder: (context) => PDFPage(PDFtitle: PDFtitle,PDFlink: PDFlink ,)),
+                         ); 
+                     
                     },
                   ),
                 ));
@@ -107,4 +119,5 @@ class _DetailsFileState extends State<DetailsFile> {
       ),
     );
   }
+
 }
